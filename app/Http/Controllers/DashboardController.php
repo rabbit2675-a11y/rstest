01206ec3976af709362c96a25984df39ca7892a5
Roger Sperfeld <rabbit2675@freenet.de>
2026-07-08 14:56:55 +0200

    Fehlerbehebung und Userliste ausgeben

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-   public function index(): View
+    public function index(): View
     {
         $users = User::all();
+
         return view('dashboard', compact('users'));
     }
 }
