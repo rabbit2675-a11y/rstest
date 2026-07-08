bf1ffba067ee970b481c2d19fcf2b3d897fa0021
Roger Sperfeld <rabbit2675@freenet.de>
2026-07-08 14:49:57 +0200

    Dashboard Controller korrigieren

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
    public function index(): View
     {
-        $users = \App\Models\User::all();
+        $users = User::all();
         return view('dashboard', compact('users'));
     }
 }
